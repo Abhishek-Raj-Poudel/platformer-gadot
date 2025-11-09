10f515db670d9daa409e78277443f73e589c43b6
Abhishek Raj Poudel <abhishekrajpoudel@gmail.com>
2025-11-09 09:06:32 +0545

    feat: Handle player flip , run & jump animation

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 150.0
 const JUMP_VELOCITY = -300.0
-
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,15 +11,28 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	# this helps chnage direction
+	var direction := Input.get_axis("move_left", "move_right")
+
+	# flip player
+	if direction > 0:
+		animated_sprite.flip_h = false 
+	elif direction < 0:
+		animated_sprite.flip_h = true
+
+	_player_animation(direction)
+
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	move_and_slide()
+
+
+func _player_animation(direction) -> void:
+	if !is_on_floor(): return animated_sprite.play('jump')
+	if direction != 0: return animated_sprite.play('run')
+	return animated_sprite.play("Idel")
